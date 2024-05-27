089a82774060a175c0c40ae4f363a1aeed9c1d86
Giovanni <giovanni.tonial@escola.pr.gov.br>
2024-05-27 15:21:03 -0300

    15:20 27/05/2024

--- src/js/script.js
+++ src/js/script.js
@@ -14,3 +14,18 @@ for (const link of links) {
         nav.classList.remove('show')
     })
 }
+// Carrocel Swiper
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination:{
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+    breakpoints:{
+        767:{
+            slidesPerView: 2,
+            setWrapperSize: true
+        }
+    }
+})
